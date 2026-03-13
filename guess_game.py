ce98804ff19e50256a77525d22db57eefdbf5f68
daisy <rtw165@gmail.com>
2026-03-13 17:40:15 +0800

    增加提示次數

--- guess_game.py
+++ guess_game.py
@@ -8,16 +8,17 @@ Created on Mon Jan 26 20:08:05 2026
 import random
 
 start,end=1,100
+count=3
 
 # 產生亂數
 x=random.randint(start,end)    
 # 提示答案
 #print(x)
 
-for i in range(10):
-    y=int(input(f"請猜一個數字({start}~{end}):"))
+for i in range(count):
+    y=int(input(f"第{i+1}/{count}次,請猜一個數字({start}~{end}):"))
     
-    # 猜對 + break
+    # 猜對 + break50
     if x==y:
         print("恭喜猜對!")   
         break
